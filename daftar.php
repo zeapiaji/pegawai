1a4de7ff5f0de0f9325ef58944da840c43640105
zeapiaji <zeapiaji@gmail.com>
2021-11-17 07:26:41 +0700

    first commit

---
+++ daftar.php
@@ -0,0 +1,11 @@
+<form action="index.php" method="POST">
+	
+	<label for="nama">Nama : </label>
+	<input type="text" name="nama">
+	<label for="no_hp">Telepon : </label>
+	<input type="text" name="no_hp">
+	<label for="id_bidang">Bidang :</label>
+		<input type="radio" name="bidang">
+	<input type="submit" name="kirim" value="kirim">
+
+</form>
---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+$koneksi = mysqli_connect("localhost","root","","tugas_pwpb");
+if ($conn) {
+	echo "tersambung";
+}
+
+
+
+ ?>
